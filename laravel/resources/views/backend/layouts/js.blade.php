dd4b992cf3455cda342107d1821c7d5cc9372b32
Thiébault Michaël <pistonman33@gmail.com>
2026-04-10 13:01:39 +0200

    Bug js for series select info and navigation by series for pending series

--- laravel/resources/views/backend/layouts/js.blade.php
+++ laravel/resources/views/backend/layouts/js.blade.php
@@ -2,54 +2,54 @@
 <script src="{{ asset('js/app.js') }}" defer></script>
 
 
-@if(Request::is('admin/finance/import'))
-<script src="{{ asset('js/finance_upload.js') }}"></script>
+@if (Request::is('admin/finance/import'))
+    <script src="{{ asset('js/finance_upload.js') }}"></script>
 @endif
-@if(Request::is('admin/finance/show*'))
-<script src="{{ asset('js/finance_update.js') }}"></script>
+@if (Request::is('admin/finance/show*'))
+    <script src="{{ asset('js/finance_update.js') }}"></script>
 @endif
-@if(Request::is('admin/finance/all*'))
-<script>
-  urlsearch = "{{URL::to('admin/finance/search')}}";
-</script>
-<script src="{{ asset('js/finance_all.js')}}"></script>
+@if (Request::is('admin/finance/all*'))
+    <script>
+        urlsearch = "{{ URL::to('admin/finance/search') }}";
+    </script>
+    <script src="{{ asset('js/finance_all.js') }}"></script>
 @endif
-@if(Request::is('admin/movies/all*'))
-<script>
-// global javascript variables
-  var urlsearch = "{{URL::to('admin/movies/search')}}";
-  var loadingImg = '{{ URL::asset('img/loading.gif') }}';
-  var csrfToken = '{{ csrf_token() }}';
-</script>
-<script src="{{ asset('js/movies_all.js')}}"></script>
+@if (Request::is('admin/movies/all*'))
+    <script>
+        // global javascript variables
+        var urlsearch = "{{ URL::to('admin/movies/search') }}";
+        var loadingImg = '{{ URL::asset('img/loading.gif') }}';
+        var csrfToken = '{{ csrf_token() }}';
+    </script>
+    <script src="{{ asset('js/movies_all.js') }}"></script>
 @endif
-@if(Request::is('admin/movies/pending*') || Request::is('tvshows/pending*'))
-<script src="{{ asset('js/movies_pending.js')}}"></script>
+@if (Request::is('admin/movies/pending*') || Request::is('admin/tvshows/pending*'))
+    <script src="{{ asset('js/movies_pending.js') }}"></script>
 @endif
-@if(Request::is('admin/movies/check/picture*') || Request::is('tvshows/check/picture*'))
-<script src="{{ asset('js/movies_pictures.js')}}"></script>
-@endif
-
-@if(Request::is('admin/stats/*'))
-  <script>
-    urlstats = "{{Request::url()}}";
-  </script>
-<script src="{{ asset('js/stats.js') }}"></script>
+@if (Request::is('admin/movies/check/picture*') || Request::is('admin/tvshows/check/picture*'))
+    <script src="{{ asset('js/movies_pictures.js') }}"></script>
 @endif
 
-@if(Request::is('admin/friends/*'))
-<script>
-    document.addEventListener('livewire:init', () => {
-        const modalEl = document.getElementById('friendModal');
-        const modal = new bootstrap.Modal(modalEl);
+@if (Request::is('admin/stats/*'))
+    <script>
+        urlstats = "{{ Request::url() }}";
+    </script>
+    <script src="{{ asset('js/stats.js') }}"></script>
+@endif
 
-        Livewire.on('open-friend-modal', () => {
-            modal.show();
+@if (Request::is('admin/friends/*'))
+    <script>
+        document.addEventListener('livewire:init', () => {
+            const modalEl = document.getElementById('friendModal');
+            const modal = new bootstrap.Modal(modalEl);
+
+            Livewire.on('open-friend-modal', () => {
+                modal.show();
+            });
+
+            Livewire.on('close-friend-modal', () => {
+                modal.hide();
+            });
         });
-
-        Livewire.on('close-friend-modal', () => {
-            modal.hide();
-        });
-    });
-</script>
+    </script>
 @endif
--- laravel/resources/views/backend/movies/pending.blade.php
+++ laravel/resources/views/backend/movies/pending.blade.php
@@ -1,81 +1,89 @@
 @extends('backend.layouts.html')
 @section('content')
-<section>
-  @include('backend.layouts.success')
-	@include('backend.layouts.error')
-  @if($current_movie)
-  <nav aria-label="Page navigation movies">
-    <ul class="pagination justify-content-end">
-      <li class="page-item {{ $previous_movie !== null ? "" : "disabled" }} ">
-        <a class="page-link" href="{{ $previous_movie !== null ? url($type.'/pending/'.$previous_movie) : "#"  }}" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-          <span class="sr-only">Previous</span>
-        </a>
-      </li>
-      <li><button type="button" class="btn btn-info">
-        movie(s) <span class="badge badge-light">{{$nb_movies}}</span>
-        </button>
-      </li>
-      <li class="page-item {{ $next_movie !== null ? "" : "disabled" }} ">
-        <a class="page-link" href="{{ $next_movie !== null ? url($type.'/pending/'.$next_movie) : "#" }}" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only">Next</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <div class="container">
-      <div class="row justify-content-center">
-          <div class="col-md-12">
-              <div class="card">
-                  <div class="card-header alert-info" align="center">
-                      <span>
-                      {{ $current_movie->title }}
-                      </span>
-                      <span class="pull-right badge badge-light">
-                      <?php echo $support; ?>
-                      </span>
-                  </div>
+    <section>
+        @include('backend.layouts.success')
+        @include('backend.layouts.error')
+        @if ($current_movie)
+            <nav aria-label="Page navigation movies">
+                <ul class="pagination justify-content-end">
+                    <li class="page-item {{ $previous_movie !== null ? '' : 'disabled' }} ">
+                        <a class="page-link"
+                            href="{{ $previous_movie !== null ? url('admin/' . $type . '/pending/' . $previous_movie) : '#' }}"
+                            aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                    <li><button type="button" class="btn btn-info">
+                            movie(s) <span class="badge badge-light">{{ $nb_movies }}</span>
+                        </button>
+                    </li>
+                    <li class="page-item {{ $next_movie !== null ? '' : 'disabled' }} ">
+                        <a class="page-link"
+                            href="{{ $next_movie !== null ? url('admin/' . $type . '/pending/' . $next_movie) : '#' }}"
+                            aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header alert-info" align="center">
+                                <span>
+                                    {{ $current_movie->title }}
+                                </span>
+                                <span class="pull-right badge badge-light">
+                                    <?php echo $support; ?>
+                                </span>
+                            </div>
 
-                  <div class="card-body">
-                      <form method="POST" action="{{ route($type.'.pending') }}" id="movieUpdateForm">
-                          @csrf
-                          <input type="hidden" name="infomovie_id" value="" />
-                          <input type="hidden" name="movie_id" value="" />
-                          <div class="card-columns">
-                          @each('movies.pending_card', $infomovies, 'movie', 'movies.no-pending_card')
-                          </div>
-                          <div class="form-group row mb-0">
-                              <div class="col-md-6 offset-md-4">
-                                <a class="btn btn-xs btn-danger" data-button-type="delete"
-                                   href="{{ url($type.'/delete/'.$current_movie->id) }}"><i class="fa fa-trash-o"></i>
-                                    Delete</a>
-                              </div>
-                          </div>
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('admin.' . $type . '.pending') }}"
+                                    id="movieUpdateForm">
+                                    @csrf
+                                    <input type="hidden" name="infomovie_id" value="" />
+                                    <input type="hidden" name="movie_id" value="" />
+                                    <div class="card-columns">
+                                        @each('backend.movies.pending_card', $infomovies, 'movie', 'movies.no-pending_card')
+                                    </div>
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6 offset-md-4">
+                                            <a class="btn btn-xs btn-danger" data-button-type="delete"
+                                                href="{{ url($type . '/delete/' . $current_movie->id) }}"><i
+                                                    class="fa fa-trash-o"></i>
+                                                Delete</a>
+                                        </div>
+                                    </div>
 
-                      </form>
-                  </div>
-                  <div class="card-footer text-muted">
-                    <form method="POST" action="{{ route($type.'.pending') }}" id="searchAnotherMovie">
-                      @csrf
-                    <div class="form-row">
-                        <div class="col-8">
-                          <input type="text" class="form-control" id="new_title" name="new_movie" value="{{$current_movie->title}}">
-                        </div>
-                        <div class="col-2">
-                          <button type="submit" class="btn btn-primary mb-2">Search</button>
+                                </form>
+                            </div>
+                            <div class="card-footer text-muted">
+                                <form method="POST" action="{{ route('admin.' . $type . '.pending') }}"
+                                    id="searchAnotherMovie">
+                                    @csrf
+                                    <div class="form-row">
+                                        <div class="col-8">
+                                            <input type="text" class="form-control" id="new_title" name="new_movie"
+                                                value="{{ $current_movie->title }}">
+                                        </div>
+                                        <div class="col-2">
+                                            <button type="submit" class="btn btn-primary mb-2">Search</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                  </form>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-  @else
-    <div class="alert alert-warning" role="alert">
-      No new movie to update from the Synology.
-    </div>
-  @endif
-</section>
+                </div>
+            </div>
+        @else
+            <div class="alert alert-warning" role="alert">
+                No new movie to update from the Synology.
+            </div>
+        @endif
+    </section>
 @endsection
