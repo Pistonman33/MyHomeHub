9a97a1a7033d1fae80d68d592517e66ddc176797
Thiébault Michaël <pistonman33@gmail.com>
2026-05-25 17:30:08 +0200

    change right directly on vps

--- docker/laravel/entrypoint.sh
+++ docker/laravel/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-# Fix permissions for storage directories (mount from host may have different ownership)
-if [ -d /var/www/storage/app ]; then
-    chown -R www-data:www-data /var/www/storage/app
-    chmod -R 775 /var/www/storage/app
-fi
-
 # Laravel storage link
 if [ ! -L /var/www/public/storage ]; then
     php /var/www/artisan storage:link || true
