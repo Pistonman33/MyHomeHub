85fcf0d841f1ac09363856619a261bbb8c2e7ec2
Thiébault Michaël <pistonman33@gmail.com>
2026-06-04 21:11:59 +0200

    Blog in french and publish language file.

--- laravel/resources/views/frontend/blog/index.blade.php
+++ laravel/resources/views/frontend/blog/index.blade.php
@@ -4,14 +4,14 @@
         <div class="text-center">
             <div
                 class="mx-auto mb-2 inline-flex rounded-lg bg-zinc-100 px-2.5 py-1 text-sm font-medium text-zinc-500 dark:bg-zinc-800/50 dark:text-neutral-400">
-                Thoughts & Learnings
+                Réflexions & Apprentissages
             </div>
-            <h1 class="font-[Space Grotesk] text-3xl font-black lg:text-5xl">Welcome to my blog</h1>
+            <h1 class="font-[Space Grotesk] text-3xl font-black lg:text-5xl">Bienvenue sur mon blog</h1>
             <h2
                 class="font-[Space Grotesk] mx-auto mt-5 max-w-2xl leading-relaxed text-balance text-zinc-500 dark:text-neutral-400">
-                Here, I share selected projects I’ve built as a web developer.<br />
-                You’ll also find tutorials and insights inspired by these projects.
-                Explore, learn, and get inspired by my hands-on work.
+                Ici, je partage une sélection de projets que j’ai réalisés en tant que développeur web.<br />
+                Vous y trouverez également des tutoriels et des réflexions inspirés de ces projets. Explorez, apprenez
+                et laissez-vous inspirer par mon expérience de terrain.
             </h2>
         </div>
         <!-- END Heading -->
