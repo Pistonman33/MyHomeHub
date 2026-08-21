4f9120b7e565ad05093a2bb07f8dba306a2c8771
Thiébault Michaël <pistonman33@gmail.com>
2026-08-21 16:23:16 +0200

    Issue with deployment changes not appears

--- docker/laravel/entrypoint.sh
+++ docker/laravel/entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 set -e
 
-copy_public_assets() {
-  if [ ! -f /var/www/public/manifest.json ] || [ ! -f /var/www/public/index.php ] || ! cmp -s /tmp/public-dist/manifest.json /var/www/public/manifest.json; then
-    echo "Initializing public volume from built image"
-    cp -a /tmp/public-dist/. /var/www/public/
-  fi
-}
-
-copy_public_assets
+echo "Synchronizing public files from the image"
+cp -a /tmp/public-dist/. /var/www/public/
 
 # Laravel storage link
 if [ ! -L /var/www/public/storage ]; then
